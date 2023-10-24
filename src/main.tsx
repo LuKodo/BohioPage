060a567329b9c94da5e33b8b7b62130a511feebc
Luis Caraballo <lcm7039@gmail.com>
2023-10-24 18:26:29 -0500

    bootstrap add

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import Router from 'preact-router'
 import { render } from 'preact';
 
-import '/src/assets/css/bootstrap.css'
-import '/src/assets/js/bootstrap.js'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
 import '/src/assets/css/main.css'
 import '/src/assets/css/carousel.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
