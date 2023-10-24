15251d7ec9d2e9476c80df4818df0b3ec64178d4
Luis Caraballo <lcm7039@gmail.com>
2023-10-24 18:03:52 -0500

    search

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
-import { render } from 'preact'
+import { render } from 'preact';
+import { useRoutes } from 'raviger';
 
 import '/src/assets/css/bootstrap.css'
 import '/src/assets/js/bootstrap.js'
@@ -6,16 +7,20 @@ import '/src/assets/css/main.css'
 import '/src/assets/css/carousel.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
-import Router from 'preact-router'
 import { Home } from './pages/home.tsx'
 import { LookUp } from './pages/lookup.tsx'
 
-export function Main() {
+const routes = {
+    '/': () => <Home />,
+    '/search': () => <LookUp />,
+}
+
+export default function Main() {
+    let route = useRoutes(routes)
     return (
-        <Router>
-            <Home path="/" />
-            <LookUp path="/search" />
-        </Router>
+        <div>
+            {route}
+        </div>
     )
 }
 
