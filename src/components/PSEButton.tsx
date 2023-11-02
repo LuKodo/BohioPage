54d18eb48254e9dc2865dd36585630b9d4fb33cb
Luis Caraballo <lcm7039@gmail.com>
2023-11-02 04:12:33 -0500

    listo

--- src/components/PSEButton.tsx
+++ src/components/PSEButton.tsx
@@ -1,15 +1,30 @@
 import { Link } from "raviger";
+import { Component } from "preact";
+import { Tooltip } from "bootstrap";
 
-export function PSEButton() {
-  return (
-    <Link href="https://www.avalpaycenter.com/wps/portal/portal-de-pagos/web/pagos-aval/resultado-busqueda/realizar-pago?idConv=00019897&origen=buscar">
-      <div className="position-fixed end-25 me-4 bottom-0 mb-4 rounded-circle">
-        <img
-          src="https://github.com/LuKodo/BohioPage/blob/main/src/assets/img/pse-logo.png?raw=true"
-          alt=""
-          width={80}
-        />
-      </div>
-    </Link>
-  );
+export class PSEButton extends Component {
+  componentDidMount() {
+    // @ts-ignore
+    new Tooltip(document.querySelector('[data-bs-toggle="tooltip"]'));
+  }
+  render() {
+    return (
+      <Link href="https://www.avalpaycenter.com/wps/portal/portal-de-pagos/web/pagos-aval/resultado-busqueda/realizar-pago?idConv=00019897&origen=buscar">
+        <div
+          className="position-fixed bg-white end-0 me-4 mb-4 rounded-circle"
+          style={{ bottom: 80, zIndex: 1000 }}
+          data-bs-toggle="tooltip"
+          data-bs-placement="top"
+          data-bs-custom-class="custom-tooltip"
+          data-bs-title="Paga aquí tu factura"
+        >
+          <img
+            src="https://github.com/LuKodo/BohioPage/blob/main/src/assets/img/pse-logo.png?raw=true"
+            alt=""
+            width={60}
+          />
+        </div>
+      </Link>
+    );
+  }
 }
